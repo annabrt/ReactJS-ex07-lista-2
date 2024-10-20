f09a97a5ca3101a37be004e374de50dda731b133
Anna Bertochi <anafbrtc@gmail.com>
2024-10-20 01:34:34 -0300

    Add files via upload

---
+++ ex07l2/src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from './components/Titulo'
+import './App.css'
+
+function App() {
+
+  return (
+      
+  
+      <Titulo/>
+ 
+  )
+}
+
+export default App
---
+++ ex07l2/src/components/Titulo.jsx
@@ -0,0 +1,8 @@
+function Titulo(){
+    return(
+        <div>
+            <h1>Aprendendo React.JS</h1>
+            <h2>Vem pro proa</h2>
+        </div>
+    )
+}
